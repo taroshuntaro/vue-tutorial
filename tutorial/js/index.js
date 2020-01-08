0e365f84fd0062c263297e228491622add5018ce
shuntaro shoji <160341ss@gmail.com>
2020-01-09 00:24:12 +0900

    宣言的レンダリング〜ユーザー入力の制御

--- tutorial/js/index.js
+++ tutorial/js/index.js
@@ -5,4 +5,49 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!'
   }
-})
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loades this page on ' + new Date().toLocaleString()
+  }
+});
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    message: 'Now you see me',
+    seenTrue: true,
+    seenFalse: false
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [{ text: 'one' }, { text: 'two' }, { text: 'three' }]
+  }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split(' ')
+        .reverse()
+        .join(' ');
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
