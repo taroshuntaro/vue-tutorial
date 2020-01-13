147244b6520d54571da4d33daf28ea37b49fdbd3
shuntaro shoji <160341ss@gmail.com>
2020-01-14 00:50:57 +0900

    コンポーネントによる構成

--- tutorial/js/index.js
+++ tutorial/js/index.js
@@ -55,5 +55,17 @@ var app6 = new Vue({
 
 // todo-item component
 Vue.component('todo-item', {
-  template: '<li>This is a todo<li>'
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
 });
